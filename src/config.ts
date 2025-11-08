99cb29e275bf8df5f25bd6e5f28c619e029f44a0
Ayush-Chaugule <ayushchaugle8975@gmail.com>
2025-11-08 16:23:59 +0530

    Initial personalized portfolio

---
+++ src/config.ts
@@ -0,0 +1,81 @@
+export const siteConfig = {
+  name: "Ayush Chaugule",
+  title: "Data Science X Statistcs X Investing",
+  description: "Portfolio website of Ayush Chaugule",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "is75xlpaw@mozmail.com",
+    linkedin: "https://www.linkedin.com/in/ayush-chaugule/",
+    twitter: "https://x.com/",
+    github: "https://github.com/Ayush-Chaugule",
+  },
+  aboutMe:
+    "I like data. I like markets. Most days I’m exploring new investment ideas, running numbers, or building something to see if a theory holds up. I enjoy digging into stats, testing what actually works, and finding patterns that turn noise into insight.",
+  skills: ["Python", "SQl", "PowerBI", "Analytical Research", "Financial Markets", "Investing Strategies"],
+  projects: [
+    {
+      name: "Differential Equation Framework for Smarter CNNs ",
+      description:
+        "Enhanced ResNet-18 with PDE-inspired DiffuNet for improved CIFAR-10 accuracy, feature smoothness, and noise robustness.",
+      link: "https://github.com/Ayush-Chaugule/Enhanced-ResNet-18-CNN-Model",
+      skills: ["Python", "Convolutional Neural Network", "Partial Differential Equations" ],
+    },
+    {
+      name: "Generating Alpha with Multihorizon Forecasting ",
+      description:
+        "Created a multi-horizon forecasting framework using regression models to generate systematic long–short trading signals for Index returns.",
+      link: "https://github.com/Ayush-Chaugule/financial-index-forecasting",
+      skills: ["Python", "Regression", "Forecasting", "Financial Markets", "Time Series"],
+    },
+    {
+      name: "CSV-Based AI Assistant",
+      description:
+        "Built a Streamlit AI assistant with PandasAI for interactive, natural-language CSV data analysis and visualization.",
+      link: "https://github.com/Ayush-Chaugule/PandasAI-CSV-Assistant",
+      skills: ["PandasAI", "Python", "CSV-Chatbot", "Streamlit"],
+    },
+  ],
+  experience: [
+    {
+      company: "IPE Journal of Management",
+      title: "Publication",
+      dateRange: "December 2024",
+      bullets: [
+        "Perception of Student regarding skill development using Artificial Intelligence",
+        
+      ],
+    },
+    {
+      company: "Bloomberg",
+      title: "Certifications",
+      dateRange: "2024-2025",
+      bullets: [
+        "Environmental, Social and Governance (ESG)",
+        "Bloomberg Market Concepts (BMC)",
+        "Bloomberg Financial Fundamentals (BFF)",
+      ],
+    },
+    {
+      company: "Jain University",
+      title: "Certificate of Presentation",
+      dateRange: "Janurary 2024",
+      bullets: [
+        "National Conference on Driving Innovation for India: 2030, Jain University Bangalore",
+        
+        
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "SVKM’s Narsee Monjee Institute of Management Studies, Bengaluru, India",
+      degree: "Bachelor of Science in Data Science",
+      dateRange: "2023 - 2026",
+      achievements: [
+        "3.55 GPA",
+        "Elected Class Representative (2 years)",
+        
+      ],
+    },
+  ],
+};
